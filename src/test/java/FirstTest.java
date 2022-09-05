528c6404dc2ec3a51feb7dcb6e9a59aea776d9c2
Hassan <hassansaleem194@gmail.com>
2022-09-05 10:09:38 +0100

    First commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+    @Test
+    public void should_work() {
+        assertEquals(true, true);
+    }
+}
